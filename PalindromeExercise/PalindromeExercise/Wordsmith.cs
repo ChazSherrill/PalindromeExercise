9125a081a49da50f8d7f8562f2dc892a7bbfc7db
Chaz Sherrill <chazsherrill@icloud.com>
2020-09-14 13:29:56 -0500

    answer

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public Wordsmith()
+        {
+        }
+        public bool IsAPalindrome(string word)
+        {
+            // int length = word.Length - 1;
+            // string drow = string.Empty; 
+            // for (; length <= 0; length--)
+            // {
+            //     drow += word[length];
+            // }
+            //
+            // return word == drow;
+
+
+            var wordArray = word.ToCharArray();
+            Array.Reverse(wordArray);
+            return word == new string(wordArray);
+
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("apple", false)]
+        [InlineData("abccba", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            var smithOWords = new Wordsmith();
 
+            var actual = smithOWords.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
